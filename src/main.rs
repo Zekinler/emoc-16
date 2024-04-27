2a9e12c7e43620ef67d82df2cff5a31f76cef90b
Olivia Burks <zekinler7@gmail.com>
2024-04-27 16:39:46 -0400

    Big refactor and the thing actually is usable now? crazy

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,104 @@
 mod memory;
 mod cpu;
 
-use cpu::CPU;
+use cpu::{Cpu, RegisterName};
+use memory::MemoryManager;
+use std::io;
+
+const ROM_SIZE: usize = 0x1000;     // 4 KiB of ROM
+const WRAM_SIZE: usize = 0xF000;    // 60 KiB of Work RAM
+
+
+/// Counts to 3 and stores the numbers at [4096]
+
+const BRANCHING_TEST_PROGRAM: [u8; 23] = [
+    0x03, 0x00, 0x10, 0x00,     // [0] Load Accumulator with [4096]
+    0x01, 0x03, 0x00, 0x01,     // [4] Load R0 with 1
+    0x10, 0x00, 0x03,           // [8] Add Accumulator with R0
+    0x06, 0x10, 0x00, 0x00,     // [11] Store at 4096, Accumulator
+    0x33, 0x00, 0x00, 0x03,     // [15] Compare Accumulator to 3
+    0x36, 0xFF, 0xEA,           // [19] Relative jump -22 bytes if comparison is not equal
+    0xFF                        // [22] Halt
+];
+
+/// Loads 0x8008 into a register, pushes it to the stack, adds 1 with the register, pops from the stack and subtracts that from the Accumulator, finally, stores the difference to [4096]
+
+const STACK_TEST_PROGRAM: [u8; 23] = [
+    0x0A, 0xFF, 0xFE,           // [0] Initialize Stack to 0xFFFE
+    0x01, 0x03, 0x80, 0x08,     // [3] Load R0 with 0x8008
+    0x0C, 0x03,                 // [7] Push R0
+    0x11, 0x03, 0x00, 0x01,     // [9] Add R0 with 1
+    0x0D, 0x03,                 // [13] Pop to R0
+    0x12, 0x00, 0x03,           // [15] Subtract Accumulator with R0
+    0x06, 0x10, 0x00, 0x00,     // [18] Store at 4096, Accumulator
+    0xFF                        // [22] Halt
+];
+
+/// Messes with state and subroutines
+
+const SUBROUTINE_TEST_PROGRAM: [u8; 27] = [
+    0x0A, 0xFF, 0xFE,           // [0] Initalize Stack to 0xFFFE
+    0x0B, 0x33, 0x33,           // [3] Push 0x3333 to stack
+    0x0B, 0x22, 0x22,           // [6] Push 0x2222 to stack
+    0x0B, 0x11, 0x11,           // [9] Push 0x1111 to stack
+    0x01, 0x03, 0x12, 0x34,     // [12] Load 0x1234 into R0
+    0x01, 0x06, 0x56, 0x78,     // [16] Load 0x5678 into R3
+    0x40, 0x04, 0x00,           // [20] Call [1024]
+    0x0B, 0x44, 0x44,           // [23] Push 0x4444 to stack
+    0xFF                        // [26] Halt
+];
+
+const TEST_SUBROUTINE: [u8; 18] = [
+    0x0B, 0x01, 0x02,           // [0] Push 0x0102 to stack
+    0x0B, 0x03, 0x04,           // [3] Push 0x0304 to stack
+    0x0B, 0x05, 0x06,           // [6] Push 0x0506 to stack
+    0x01, 0x03, 0x07, 0x08,     // [9] Load 0x0708 into R0
+    0x01, 0x06, 0x09, 0x0A,     // [13] Load 0x090A into R3
+    0x42                        // [17] Return
+];
+
 
 fn main() {
-	let mut cpu = CPU::new(0x20000000);	// 512 MiB ram
+    let mut cpu = Cpu::new();
 
-	// This counts to 3 and stores it into the address location 256
-	cpu.ram.load(0, &[
-		0x16, 0x00, 0x00, 0x00, 0x01,	// Move into GA, 1
-		0x17, 0x00, 0x00, 0x00, 0x03,	// Move into GB, 3
-		0x21, 0x00, 0x00, 0x01, 0x00,	// Load to ACC from Adrloc. 256
-		0x00, 0x16,						// Add ACC and GA
-		0x31, 0x00, 0x00, 0x01, 0x00,	// Store from ACC to Adrloc. 256
-		0x09, 0x17,						// Unsigned compare ACC and GB
-		0x54, 0xED,						// Jump a byte if not equal, -19 bytes (0xED is -19 as a signed byte)
-		0xFF,							// Halt
-	]);
+    let mut memory = MemoryManager::new(ROM_SIZE + WRAM_SIZE);
 
-	cpu.run();
+    let rom_pointer = memory.add_device(String::from("ROM"), memory::DeviceAccessMode::ReadOnly, 0..ROM_SIZE);
 
-	println!("256:4 = {}", cpu.ram.read32(256));	// Printout Adrloc. 256 after it's done
+    let wram_pointer = memory.add_device(String::from("WRAM"), memory::DeviceAccessMode::ReadWrite, rom_pointer.end..(rom_pointer.end + WRAM_SIZE));
+
+    memory.write_bytes(0, &SUBROUTINE_TEST_PROGRAM);
+    memory.write_bytes(1024, &TEST_SUBROUTINE);
+
+    let mut cycle = 0;
+
+    while !cpu.is_halted() {
+        let pc = cpu.read_register32(RegisterName::ProgramCounterWH);
+        
+        println!("{} [PC]{:04X},4 = 0x{:08X}", cycle, pc, memory.read32(pc as usize, true));
+
+        let mut dummy = String::new();
+
+        io::stdin()
+            .read_line(&mut dummy)
+            .expect("Failed to read line");
+
+        
+        cpu.clock(&mut memory);
+
+        cycle += 1;
+
+        println!("{} R0 = 0x{:04X}", cycle, cpu.read_register16(RegisterName::R0H));
+        println!("{} R3 = 0x{:04X}", cycle, cpu.read_register16(RegisterName::R3H));
+        println!("{} BP = 0x{:04X}", cycle, cpu.read_register32(RegisterName::BasePointerWH));
+
+        let sp = (cpu.read_register32(RegisterName::StackPointerWH) + 2) as usize;
+
+        println!("{} [SP + 2]{:04X},2 = 0x{:04X}", cycle, sp, memory.read16(sp, true));
+        println!("");
+    }
+
+    println!("- Halted");
+
+    println!("- [4096],2 = 0x{:04X}", memory.read16(0x1000, true));
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,91 +1,171 @@
-pub struct Memory {
-  data: Box<[u8]>,
-  size: usize,
+#![allow(dead_code)]
+
+pub enum DeviceAccessMode {
+    ReadWrite,
+    ReadOnly,
+    WriteOnly,
 }
 
-impl Memory {
-  pub fn new(size: usize) -> Memory {
-    Memory {
-      data: vec![b'\0'; size].into_boxed_slice(),
-      size: size,
-    }
-  }
+struct Device {
+    name: String,
+    access_mode: DeviceAccessMode,
+    range: std::ops::Range<usize>,
+}
 
-  pub fn size(&self) -> usize {
-    self.size
-  }
+pub struct MemoryManager {
+    data: Box<[u8]>,
+    devices: Vec<Device>,
+}
 
-  pub fn load(&mut self, index: usize, data: &[u8]) {
-    for (i, byte) in data.iter().enumerate() {
-      self.data[index + i] = *byte;
-    }
-  }
-
-  pub fn read_bytes(&self, index: usize, amount: usize) -> Vec<u8> {
-    let mut data = vec![b'\0'; amount];
-
-    let mut i = 0;
-    while i < amount {
-        data[i] = self.read8(index + i);
-        i += 1;
+impl MemoryManager {
+    pub fn new(size: usize) -> MemoryManager {
+        if size < 1 {
+            panic!("Can't initialize with a size of 0");
+        }
+        
+        MemoryManager {
+            data: vec![0x00; size].into_boxed_slice(),
+            devices: Vec::new(),
+        }
     }
 
-    data
-  }
+    pub fn add_device(&mut self, name: String, access_mode: DeviceAccessMode, range: std::ops::Range<usize>) -> std::ops::Range<usize> {
+        if range.start > self.data.len() {
+            panic!("Start of range: {}, is larger than size of memory: {}", range.start, self.data.len());
+        }
 
+        if range.end > self.data.len() {
+            panic!("End of range: {}, is larger than size of memory: {}", range.end, self.data.len());
+        }
+        
+        if range.start > range.end {
+            panic!("Start of range: {}, is larger than end of range: {}", range.start, range.end);
+        }
 
-  pub fn read8(&self, index: usize) -> u8 {
-    if index < self.size {
-      return self.data[index]
-    } else {
-      return 0
+        if range.end == range.start {
+            panic!("End of range: {}, is equal to start of range: {}", range.end, range.start);
+        }
+
+        for device in self.devices.iter() {
+            if name == device.name {
+                panic!("Device with name: {} already exists", name);
+            }
+
+            if range.start >= device.range.end || (range.start < device.range.start && range.end <= device.range.start) {
+                continue;
+            }
+
+            panic!("Range: {}..{}, is already covered by a different device: \"{}\" with range: {}..{}", range.start, range.end, device.name, device.range.start, device.range.end);
+        }
+        
+        let ret_range = range.clone();
+
+        let device = Device {
+            name,
+            access_mode,
+            range,
+        };
+
+        self.devices.push(device);
+
+        ret_range
     }
-  }
 
-  pub fn read16(&self, index: usize) -> u16 {
-    let a = if index < self.size { (self.data[index] as u16) << 8 } else { 0 };
-    let b = if index + 1 < self.size { self.data[index + 1] as u16 } else { 0 };
+    pub fn size(&self) -> usize {
+        self.data.len()
+    }
 
-    a | b
-  }
+    fn get_device_from_index(&self, index: usize) -> Option<&Device> {
+        for device in self.devices.iter() {
+            if device.range.contains(&index) {
+                return Some(device);
+            }
+        }
+        None
+    }
+
+    pub fn write_bytes(&mut self, index: usize, data: &[u8]) {
+        for (i, byte) in data.iter().enumerate() {
+            self.data[index + i] = *byte;
+        }
+    }
+
+    pub fn read_bytes(&self, index: usize, amount: usize) -> &[u8] {
+        &self.data[index..index + amount]
+    }
+
+
+    pub fn read8(&self, index: usize, force: bool) -> u8 {
+        if !force {
+            if let Some(device) = self.get_device_from_index(index) {
+                match device.access_mode {
+                    DeviceAccessMode::WriteOnly => return 0,
+                    _ => (),
+                }
+            }
+        }
+        
+        if index >= self.data.len() { 0 }
+        else                        { self.data[index] }
+    }
+
+    pub fn read16(&self, index: usize, force: bool) -> u16 {
+        let a = self.read8(index, force) as u16;
+        let b = self.read8(index + 1, force) as u16;
+
+        a << 8 | b
+    }
   
-  pub fn read32(&self, index: usize) -> u32 {
-    let a = if index < self.size { (self.data[index] as u32) << 24 } else { 0 };
-    let b = if index + 1 < self.size { (self.data[index + 1] as u32) << 16 } else { 0 };
-    let c = if index + 2 < self.size { (self.data[index + 2] as u32) << 8 } else { 0 };
-    let d = if index + 3 < self.size { self.data[index + 3] as u32 } else { 0 };
+    pub fn read32(&self, index: usize, force: bool) -> u32 {
+        let a = self.read16(index, force) as u32;
+        let b = self.read16(index + 2, force) as u32;
 
-    a | b | c | d
-  }
+        a << 16 | b
+    }
+
+    pub fn read64(&self, index: usize, force: bool) -> u64 {
+        let a = self.read32(index, force) as u64;
+        let b = self.read32(index + 4, force) as u64;
+
+        a << 32 | b
+    }
+
+    pub fn write8(&mut self, index: usize, value: u8, force: bool) {
+        if !force {
+            if let Some(device) = self.get_device_from_index(index) {
+                match device.access_mode {
+                    DeviceAccessMode::ReadOnly => return,
+                    _ => (),
+                }
+            }
+        }
+
+        if index < self.size() {
+            self.data[index] = value;
+        }
+    }
+
+    pub fn write16(&mut self, index: usize, value: u16, force: bool) {
+        let a = (value >> 8) as u8;
+        let b = value as u8;
+
+        self.write8(index, a, force);
+        self.write8(index + 1, b, force);
+    }
   
+    pub fn write32(&mut self, index: usize, value: u32, force: bool) {
+        let a = (value >> 16) as u16;
+        let b = value as u16;
 
-  pub fn write8(&mut self, index: usize, value: u8) {
-    if index < self.size {
-      self.data[index] = value;
+        self.write16(index, a, force);
+        self.write16(index + 2, b, force);
     }
-  }
 
-  pub fn write16(&mut self, index: usize, value: u16) {
-    if index < self.size {
-      self.data[index] = (value >> 8) as u8;
+    pub fn write64(&mut self, index: usize, value: u64, force: bool) {
+        let a = (value >> 32) as u32;
+        let b = value as u32;
+
+        self.write32(index, a, force);
+        self.write32(index + 4, b, force);
     }
-    if index + 1 < self.size {
-      self.data[index + 1] = value as u8;
-    }
-  }
-  
-  pub fn write32(&mut self, index: usize, value: u32) {
-    if index < self.size {
-      self.data[index] = (value >> 24) as u8;
-    }
-    if index + 1 < self.size {
-      self.data[index + 1] = (value >> 16) as u8;
-    }
-    if index + 2 < self.size {
-      self.data[index + 2] = (value >> 8) as u8;
-    }
-    if index + 3 < self.size {
-      self.data[index + 3] = value as u8;
-    }
-  }
 }
